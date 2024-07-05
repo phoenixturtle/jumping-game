ee215cb0e988f300aa96f8414afa5b6e0d11f2d3
stacey_key <stacy.kar94@gmail.com>
2024-07-06 02:50:28 +0300

    Отмените все незафиксированные изменения (если они есть) с помощью команды:

---
+++ script.js
@@ -0,0 +1,11 @@
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+
+function jump(){
+if(character.classList != "animate"){
+character.classList.add("animate");
+}
+setTimeout(function(){
+character.classList.remove("animate")
+},500);
+}
